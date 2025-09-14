a0448a40f3654ec4679172fe1ffbb4ff8cf5bf05
Motgy <reggasdahmane@gmail.com>
2025-09-14 15:50:55 -0400

    typo fix

--- src/app/components/Wrapper.jsx
+++ src/app/components/Wrapper.jsx
@@ -1,5 +1,5 @@
 const Wrapper = ({ children }) => {
-    return <div class="filter drop-shadow-xl">
+    return <div className="filter drop-shadow-xl">
         {/* pas bsoin dfaire grand chose pr mobile c deja bien centre */}
         <div className="w-[340px] bg-gray-900 text-white rounded-2xl p-4 shadow-lg">
             {children}
